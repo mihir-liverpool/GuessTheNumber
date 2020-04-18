6cce808295de7996f2404dec7261723c62e255ca
shemihir <shemihir@u22ee1abb706256.ant.amazon.com>
2020-04-18 04:54:22 -0400

    "Making a string change to make it more user friendly"

--- Guessthenumber.py
+++ Guessthenumber.py
@@ -5,7 +5,7 @@ print(secret)
 guess = 0
 tries = 0
 while guess != secret:
-    guess = int(input("make a guess: "))
+    guess = int(input("make a guess between 1 to 100: "))
     tries = tries + 1
     if guess > secret:
         print('that is too high !!')
