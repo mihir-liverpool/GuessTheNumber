14c2a6da3db4af8da6d1d34370b4e27c091bfd9c
shemihir <shemihir@u22ee1abb706256.ant.amazon.com>
2020-04-18 04:47:45 -0400

    Adding all the files for the project

---
+++ Guessthenumber.py
@@ -0,0 +1,16 @@
+import random
+
+secret = random.randrange(0, 100)
+print(secret)
+guess = 0
+tries = 0
+while guess != secret:
+    guess = int(input("make a guess: "))
+    tries = tries + 1
+    if guess > secret:
+        print('that is too high !!')
+    elif guess < secret:
+        print('that is too low !!')
+    else:
+        print('You got it !!')
+print('You took', tries, 'tries')
